e84428a1019f2e1838751dd2e28741992bcfe16b
exZon exZon <yashpatel5566789>
2020-09-02 18:02:19 +0530

    final commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCfT1cp_oEyAiW4qVzPAOrO77h62AqWe74",
+    authDomain: "covid-19-tracker-d5d56.firebaseapp.com",
+    databaseURL: "https://covid-19-tracker-d5d56.firebaseio.com",
+    projectId: "covid-19-tracker-d5d56",
+    storageBucket: "covid-19-tracker-d5d56.appspot.com",
+    messagingSenderId: "810927448095",
+    appId: "1:810927448095:web:00668765a2dcbd1421352a",
+    measurementId: "G-DHZEL0V3E5"
+  };
